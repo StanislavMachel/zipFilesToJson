ba0764507867b87bcba0888be89b86e0e8d27f2d
Stanislav Machel <stanislav.machel@gmail.com>
2018-05-07 21:53:48 +0300

    Add Razor Page, create ItemTree from ZipFile entries and Serialize to JSON

---
+++ ControlPanel.Web/IZipFileToTreeService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ControlPanel.Web.Entities;
+using Microsoft.AspNetCore.Http;
+
+namespace ControlPanel.Web
+{
+    public interface IZipFileToTreeService
+    {
+        TreeItem ToTreeItem(IFormFile formFile);
+    }
+}
