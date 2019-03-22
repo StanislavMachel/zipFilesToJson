62847a7271ac8cd945457335e9db577d0533efdf
Stanislav Machel <stanislav.machel@gmail.com>
2019-03-22 21:38:20 +0200

    Remove unnecessary things

--- DataManagementSystem.Core/Entities/ZipFileJson.cs
+++
@@ -1,9 +0,0 @@
-﻿using DataManagementSystem.Core.Entities.Common;
-
-namespace DataManagementSystem.Core.Entities
-{
-    public class ZipFileJson : BaseEntity
-    {
-        public string ZipFileStructureJson { get; set; }
-    }
-}
